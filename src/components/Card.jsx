4660ec2d7acd0ebbad511782e3a8905fb0ae2336
Ly <lydialawli@gmail.com>
2019-09-18 16:41:59 +0700

    added icons, styled card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,57 +1,29 @@
 import React from 'react';
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonIcon, IonItem, IonLabel } from '@ionic/react';
+import '../styles/card.css';
 
 class Card extends React.Component {
+    state = {
+        quote: 'Education is not the learning of facts, but the training of the mind to think',
+        author: 'Albert Einstein',
+        title: `Today's quote`
+    }
+
     render() {
+
         return (
-            <IonContent>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-                        <IonCardTitle>Card Title</IonCardTitle>
-                    </IonCardHeader>
 
-                    <IonCardContent>
-                        Keep close to Nature's heart... and break clear away, once in awhile,
-                        and climb a mountain or spend a week in the woods. Wash your spirit clean.
-      </IonCardContent>
-                </IonCard>
+            <IonCard className='card'>
+                <IonCardHeader>
+                    <IonCardTitle className="text">{this.state.title}</IonCardTitle>
+                </IonCardHeader>
 
-                <IonCard>
-                    <IonItem>
-                        <IonIcon name="pin" slot="start" />
-                        <IonLabel>ion-item in a card, icon left, button right</IonLabel>
-                        <IonButton fill="outline" slot="end">View</IonButton>
-                    </IonItem>
+                <IonCardContent className="quote text">
+                    "{this.state.quote}"
+                    </IonCardContent>
+                <IonCardSubtitle className="author text">{this.state.author}</IonCardSubtitle>
+            </IonCard>
 
-                    <IonCardContent>
-                        This is content, without any paragraph or header tags,
-                        within an ion-cardContent element.
-      </IonCardContent>
-                </IonCard>
-
-                <IonCard>
-                    <IonItem href="#" class="activated">
-                        <IonIcon name="wifi" slot="start" />
-                        <IonLabel>Card Link Item 1 .activated</IonLabel>
-                    </IonItem>
-
-                    <IonItem href="#">
-                        <IonIcon name="wine" slot="start" />
-                        <IonLabel>Card Link Item 2</IonLabel>
-                    </IonItem>
-
-                    <IonItem class="activated">
-                        <IonIcon name="warning" slot="start" />
-                        <IonLabel>Card Button Item 1 .activated</IonLabel>
-                    </IonItem>
-
-                    <IonItem>
-                        <IonIcon name="walk" slot="start" />
-                        <IonLabel>Card Button Item 2</IonLabel>
-                    </IonItem>
-                </IonCard>
-            </IonContent>
         )
     }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 import React from 'react';
 import Card from '../components/Card.jsx';
+import '../styles/global.css';
+import MdHeart from 'react-ionicons/lib/MdHeart';
+import MdShare from 'react-ionicons/lib/MdShare';
+import MdChatbubbles from 'react-ionicons/lib/MdChatbubbles';
 
 class Home extends React.Component {
   render() {
@@ -8,12 +12,24 @@ class Home extends React.Component {
       <IonPage>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>Ionic Blank</IonTitle>
+            <IonTitle>Quotes App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">
-          <Card />
+
+        <IonContent>
+          <IonGrid>
+            <IonCol class="ion-align-items-center" size="6">
+              <Card />
+              <IonRow class="ion-align-items-end">
+                <MdHeart fontSize="30px" color="black" />
+                <MdChatbubbles fontSize="30px" color="black" />
+                <MdShare fontSize="30px" color="black" />
+              </IonRow>
+            </IonCol>
+          </IonGrid>
         </IonContent>
+
+
       </IonPage>
     )
   }
